03c62fea44d0e4312b0ffa4df563ca78c9d948fd
Yashhh <yashkadam2900@gmail.com>
2025-12-22 01:13:58 +0530

    Reset repository: remove all previous files

--- core/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    filename="jarvis_debug.log",
-    level=logging.DEBUG,
-    format="%(asctime)s | %(levelname)s | %(message)s"
-)
-
-logger = logging.getLogger("JARVIS")
